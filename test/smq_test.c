ddfa649e498a51cbb13ebcb3f1a76b12626437b6
Kyle Isom <coder@kyleisom.net>
2013-01-09 19:13:13 -0700

    start writing unit tests

--- test/smq_test.c
+++ test/smq_test.c
@@ -25,170 +25,90 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <smq.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+#include "smq.h"
 
 #define BUFSZ                   32
 #define NMSG                    32
-static struct s_msgqueue       *mq;
-extern const char               LIBSMQ_VERSION[];
 
-static void     *load_data(void *);
-static void     *read_data(void *);
-static void      ms_sleep(short);
-void     sthrd_test(void);
+
+static void
+test_simple_smq(void)
+{
+        struct smq      *msgq;
+        struct smq_msg  *message;
+        int              retval = 0;
+
+        msgq = smq_create();
+        CU_ASSERT(NULL != msgq);
+
+        retval = smq_destroy(msgq);
+        switch (retval) {
+        case 0:
+                break;
+        case EBUSY:
+                fprintf(stderr, "[!] mutex is already locked.\n");
+                break;
+        case EINVAL:
+                fprintf(stderr, "[!] invalid mutex.\n");
+                break;
+        default:
+                fprintf(stderr, "[!] invalid state: error = %d\n", retval);
+                abort();
+        }
+        CU_ASSERT(0 == retval);
+}
+
+/*
+ * suite set up functions
+ */
+int initialise_smq_test()
+{
+    return 0;
+}
+
+int
+cleanup_smq_test()
+{
+    return 0;
+}
+
+void destroy_test_registry()
+{
+    CU_cleanup_registry();
+    exit(CU_get_error());
+}
 
 int
 main(void)
 {
-        void            *thrdv;
-        pthread_t        shover;
-        pthread_t        pusher;
+    CU_pSuite smq_suite = NULL;
+    unsigned int fails = 0;
 
-        printf("[+] testing %s\n", LIBSMQ_VERSION);
-        printf("[+] start single threaded test\n");
-        sthrd_test();
+    printf("starting tests for smq...\n");
 
-        ms_sleep(1000);
-        printf("[+] start multithreaded test\n");
-        mq = msgqueue_create();
+    if (! CUE_SUCCESS == CU_initialize_registry()) {
+        fprintf(stderr, "error initialising CUnit test registry!\n");
+        return EXIT_FAILURE;
+    }
 
-        printf("[+] pusher robot starts\n");
-        pthread_create(&pusher, NULL, read_data, NULL);
-        ms_sleep(10);
-        printf("[+] shover robot starts\n");
-        pthread_create(&shover, NULL, load_data, NULL);
+    /* set up the suite */
+    smq_suite = CU_add_suite("smq_tests", initialise_smq_test,
+                                            cleanup_smq_test);
+    if (NULL == smq_suite)
+        destroy_test_registry();
 
-        pthread_join(pusher, &thrdv);
-        printf("[+] pusher robot finishes.\n");
-        pthread_join(shover, &thrdv);
-        printf("[+] shover robot finishes.\n");
+    /* add tests */
+    if (NULL == CU_add_test(smq_suite, "single thread test",
+                            test_simple_smq))
+        destroy_test_registry();
 
-        printf("[+] destroying message queue\n");
-        return msgqueue_destroy(mq);
-}
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    fails = CU_get_number_of_tests_failed();
 
-
-static void
-ms_sleep(short ms)
-{
-        struct timespec  timeo;
-        struct timespec  unslept;
-        int              sec = 0;
-
-        if (ms >= 1000) {
-                sec = ms / 1000;
-        }
-
-        timeo.tv_sec = sec;
-        timeo.tv_nsec = (ms % 1000) * 1000;
-        while (0 != nanosleep((const struct timespec *)&timeo, &unslept)) {
-                timeo = unslept;
-        }
-}
-
-void *
-load_data(void *junk)
-{
-        char     buf[BUFSZ+1];
-        size_t   nsent = 0;
-        ssize_t  rdsz;
-        size_t   i = 0;
-        int      error = 1;
-        FILE    *dr;
-
-        junk = NULL;
-        dr = fopen("/dev/urandom", "r");
-        assert(NULL != dr);
-
-        while (nsent < NMSG) {
-                rdsz = fread(buf, sizeof(char), BUFSZ, dr);
-                if (rdsz < BUFSZ)
-                        printf("<load data> only read %lu bytes\n",
-                            (long unsigned)rdsz);
-                for (i = 0; i < BUFSZ; i++) {
-                        if (buf[i] == '\0')
-                                buf[i] = ' ';
-                }
-                error = 1;
-                while (error) {
-                        error = msgqueue_enqueue(mq, (uint8_t *)buf, BUFSZ);
-                        if (!error) {
-                                printf("[+] <load_data> enqueue %d bytes\n",
-                                    BUFSZ);
-                        } else {
-                                printf("[!] <load_data> failed to enqueue "
-                                    "%d bytes\n", BUFSZ);
-                                ms_sleep(25);
-                        }
-                }
-                nsent++;
-        }
-
-        printf("[+] <shover> sent %lu messages\n", (long unsigned)nsent);
-        fclose(dr);
-        printf("[+] shover robot is almost finished...\n");
-        pthread_exit(NULL);
-        printf("[+] shover robot is terminating its thread!\n");
-        return junk;
-}
-
-
-void *
-read_data(void *junk)
-{
-        struct s_message *msg;
-        size_t  cnt = 0;
-        size_t  last = 0;
-
-        while (cnt < NMSG) {
-                msg = msgqueue_dequeue(mq);
-                if (NULL == msg) {
-                        ms_sleep(10);
-                        continue;
-                }
-                printf("[+] (%4lu) received %lu bytes (msg seq: %lu)\n",
-                    (long unsigned)cnt,
-                    (long unsigned)strlen((char *)msg->msg),
-                    (long unsigned)msg->seq);
-
-                if (last != msg->seq-1) {
-                        printf("[-] <read_data> sequence miss\n");
-                } else if (msg->msglen != BUFSZ) {
-                        printf("[-] <read_data> invald read size:\n");
-                        printf("\tmsg data: '%s'\n", msg->msg);
-                }
-                cnt++;
-                last = msg->seq;
-                free(msg->msg);
-                msg->msg = NULL;
-                free(msg);
-                msg = NULL;
-        }
-        printf("pusher robot is almost finished...\n");
-        pthread_exit(NULL);
-        return junk;
-}
-
-
-void
-sthrd_test()
-{
-        struct s_msgqueue       *msq;
-        struct s_message        *msg = NULL;
-
-        msq = msgqueue_create();
-        if (0 != msgqueue_enqueue(msq, (uint8_t *)"foo", strlen("foo")))
-                printf("[!] enqueue 1 failed\n");
-        if (0 != msgqueue_enqueue(msq, (uint8_t *)"bar", strlen("bar")))
-                printf("[!] enqueue 2 failed\n");
-        if (NULL == (msg = msgqueue_dequeue(msq)))
-                printf("[!] dequeue failed\n");
-        else {
-                printf("[+] first message: %s (seq=%lu)\n", msg->msg,
-                    (long unsigned)msg->seq);
-        }
-
-        if (0 != msgqueue_destroy(msq))
-                printf("[!] failed to destroy message queue\n");
-        printf("[+] finishing single threaded test\n");
+    CU_cleanup_registry();
+    return fails;
 }
